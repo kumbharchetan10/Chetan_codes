7619a1fee76d88365e6c8b725bb108fa05dce764
kumbharchetan10 <76802558+kumbharchetan10@users.noreply.github.com>
2020-12-31 11:38:28 +0530

    Coding Started

---
+++ chetan.py
@@ -0,0 +1,18 @@
+X = 'Welcome to my World!'
+Print(X)
+
+n = 7
+for i in range (1,7):
+	print(' '*n, end = " ")
+	print('* '*(i))
+	n = n - 1
+
+i = 1
+while i < 12:
+	print('chetan', end =" ")
+	i = i + 1
+	print()
+
+n = 10
+for i in range(n,0,-1):
+	print("*"*i)
